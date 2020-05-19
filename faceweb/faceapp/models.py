6ebe7fd277758b74959c509b71482556e9b5e8df
Dheeraj2407 <dheerajvijayakumar19@gmail.com>
2020-05-19 12:04:13 +0530

    Added model

--- faceweb/faceapp/models.py
+++ faceweb/faceapp/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subjects(models.Model):
+    text = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
