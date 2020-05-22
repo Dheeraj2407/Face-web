bd3a52d9c32db1fd3220b65918c511606f9169aa
Dheeraj2407 <dheerajvijayakumar19@gmail.com>
2020-05-22 13:57:13 +0530

    Added Model Structure

--- faceweb/faceapp/models.py
+++ faceweb/faceapp/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Subjects(models.Model):
-    text = models.CharField(max_length=50)
-    date_added = models.DateTimeField(auto_now_add=True)
-
+class ClassRoom(models.Model):
+    classRoom = models.IntegerField()
     def __str__(self):
-        """Return a string representation of the model."""
-        return self.text
+        """Return string representation of the model."""
+        return str(self.classRoom)
+
+class TeacherClass(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    classRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50, default='')
+
+class StudentClass(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    classRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+
+class AttendanceLogs(models.Model):
+    student = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(TeacherClass, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+
+class TimeTable(models.Model):
+    ClassRoom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    day = models.CharField(max_length=50, default='')
+    hour = models.IntegerField(default=0)
+    subject = models.CharField(max_length=50, default='')
